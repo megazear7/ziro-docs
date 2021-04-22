57628090b58fcf4874e64b4c3a48f5d44c50292d
Alex Lockhart <alexlockahrt7@gmail.com>
2021-04-22 06:23:16 -0400

    bumped service worker

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,4 +1,4 @@
-var appCacheName = 'app-cache-v5';
+var appCacheName = 'app-cache-v6';
 var cacheNames = [appCacheName];
 var offlinePage = '/offline.html';
 var offlineFragment = '/offline.fragment.html';
