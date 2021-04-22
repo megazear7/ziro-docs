549de50cc4ca9cbe703df6fe27e3aac118d7579f
Alex Lockhart <alexlockahrt7@gmail.com>
2021-04-22 06:24:33 -0400

    Fixed image sizes

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,4 +1,4 @@
-var appCacheName = 'app-cache-v6';
+var appCacheName = 'app-cache-v7';
 var cacheNames = [appCacheName];
 var offlinePage = '/offline.html';
 var offlineFragment = '/offline.fragment.html';
